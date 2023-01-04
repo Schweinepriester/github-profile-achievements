1e8cd3c03c75a27fdf276f0ec86622e4ba3ff510
Akshat verma <99114167+akshatvermavi@users.noreply.github.com>
2023-01-04 14:05:13 +0530

    new commit by avi

--- console.js
+++ console.js
@@ -1,2 +1,7 @@
 console.log('abc');
 console.log(1);
+console.log(true);
+console.log(null);
+console.log(undefined);
+console.log([1, 2, 3, 4]); // array inside log
+console.log({a:1, b:2, c:3}); // object inside log
