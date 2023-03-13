79195b3f7ba2a54493d1e002fcbb1647fa103939
Brayam Huaman <74569086+brayamhuaman@users.noreply.github.com>
2023-03-12 21:54:17 -0500

    Create

---
+++ short.js
@@ -0,0 +1,6 @@
+ujutsu Kaisen Cero
+let marcasLaptops = ["Apple", "Dell", "Lenovo", "HP", "Acer", "Asus"];
+
+// Accediendo a los elementos del array
+console.log("La primera marca de laptops es " + marcasLaptops[0]); // Apple
+console.log("La tercera marca de laptops es " + marcasLaptops[2]); // Lenovo
