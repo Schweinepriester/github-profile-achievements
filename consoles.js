057e483bb256af9e3d7bb88a5d1b775c9c4052e5
Brayam Huaman <74569086+brayamhuaman@users.noreply.github.com>
2023-03-12 21:17:21 -0500

    Update consoles.js Days of Week
    Days of week spanish

--- consoles.js
+++ consoles.js
@@ -1,1 +1,8 @@
 console.log("Hola mundo");
+// Creamos un array con los nombres de los días de la semana
+let diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
+// Recorremos el array con un bucle for y mostramos cada día por consola
+for (let i = 0; i < diasSemana.length; i++) {
+  console.log(diasSemana[i]);
+}
