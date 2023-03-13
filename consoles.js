6be866d96cc619b55fc9a7eda7d812f984e3efa2
Brayam Huaman <74569086+brayamhuaman@users.noreply.github.com>
2023-03-12 21:20:08 -0500

    Update consoles.js

--- consoles.js
+++ consoles.js
@@ -6,3 +6,12 @@ let diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado
 for (let i = 0; i < diasSemana.length; i++) {
   console.log(diasSemana[i]);
 }
+
+
+// Creamos un array con los nombres de los meses del año
+let mesesAnio = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// Recorremos el array con un bucle for y mostramos cada mes por consola
+for (let i = 0; i < mesesAnio.length; i++) {
+  console.log(mesesAnio[i]);
+}
