135fe47d1108faf2659995468cea78e09ab5c08f
Brayam Huaman <74569086+brayamhuaman@users.noreply.github.com>
2023-03-12 21:44:54 -0500

    Update console.js
    CODIGO LAPTST

--- console.js
+++ console.js
@@ -40,3 +40,14 @@ let minutosActuales = fechaActual.getMinutes();
 
 console.log("La hora actual es " + horaActual + ":" + minutosActuales);
 
+
+
+// CODIGO LAPTOPS
+
+let marcasLaptops = ["Apple", "Dell", "Lenovo", "HP", "Acer", "Asus"];
+
+// Accediendo a los elementos del array
+console.log("La primera marca de laptops es " + marcasLaptops[0]); // Apple
+console.log("La tercera marca de laptops es " + marcasLaptops[2]); // Lenovo
+
+
