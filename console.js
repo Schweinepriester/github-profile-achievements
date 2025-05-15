f63e64106e319bb0ed8e5d2f5c5f503d800109fc
thabet1thabet1 <thabetchk2@gmail.com>
2025-05-15 21:57:11 +0100

    Update console.js

--- console.js
+++ console.js
@@ -1,2 +1,4 @@
 console.log("hello wrld")
 console.log("hello to all people out there") //hell yeah
+console.log("hello omg")
+console.log("hello to all yessssssssssssssssssss out there") //hell yeah
