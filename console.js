b196ec6922d6c62749938ae73e2edc5f93d56e50
Arial12 <jestano_230000001603@uic.edu.ph>
2025-05-29 01:36:01 +0800

    Update console.js

--- console.js
+++ console.js
@@ -1,6 +1,6 @@
 //Hello skibid toilet rizz
 
-console.log("WOw grape")
-console.log("WOWOWOWOADOAKSODKASODKAOSDKAOSDKOAKDOASKODAOSDM ARRGHGHGHGHGHGe")
+console.log("WOw grapasdase")
+console.log("WOWOWOasdasdWOADOAKSODKASODKAOSDKAOSDKOAKDOASKODAOSDM ARRGHGHGHGHGHGe")
 
 console.log("I am sorry I am trying something here.")
