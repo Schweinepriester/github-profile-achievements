aa678049c4013fff8fa15ab112955f3f1fa26362
Aditi Singh <123637953+kumariaditi52@users.noreply.github.com>
2025-08-19 19:05:41 +0530

    Create console.js

---
+++ console.js
@@ -0,0 +1,5 @@
+// --- Basic Logging ---
+console.log("Hello, World! 🌍");
+console.info("This is an informational message.");
+console.warn("Warning! Something might need attention.");
+console.error("Error! Something went wrong.");
